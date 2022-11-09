10d75e29b2f3520dffa9843345854dd798184722
saltchess <saltrainchess@gmail.com>
2022-11-10 06:43:49 +0800

    try

--- js/check-edit.js
+++ js/check-edit.js
@@ -1,3 +1,18 @@
+const { readFile } = require("node:fs/promises");
+const axios = require("axios");
+const wiki_url = require("./wiki_url");
+const { resolve } = require("path");
 (async () => {
-	;
+	try {
+		const filePath = process.argv[2]
+		const [wiki, contents] = await Promise.all([
+			axios.get(wiki_url({ titles: "Template:" + filePath.match(/[^\/\\]+$/)[0] + ".json" }))
+				.then(v => v.data.query.pages[0].revisions[0].content),
+			readFile(resolve(filePath), { encoding: 'utf8' })
+		])
+		console.log(String(wiki != contents))
+	} catch (error) {
+		console.error("error")
+		process.exit(1)
+	}
 })()
