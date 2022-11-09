38471a364c63549c9caff441385556da5edd3a37
saltchess <saltrainchess@gmail.com>
2022-11-10 04:05:55 +0800

    newer

---
+++ js/test.js
@@ -0,0 +1,3 @@
+console.log("https://static-bin.lowiro.com/serve/arcaea_4.1.3c.apk?token=3Ad7D3EXvx7K5l4ZzS9Vc5sv8LxfKqEj7D7bBuYLXUoKQS1LNpKRdtwhNcQPNjmu9")
+console.log("4.1.4")
+console.log("result")
