df37d72f50d5b673dbfa4d8f5697a3ece711fd62
saltchess <saltrainchess@gmail.com>
2022-11-10 20:32:20 +0800

    dead loop

--- js/check-update.js
+++ js/check-update.js
@@ -1,28 +1,35 @@
 const axios = require("axios");
 const wiki_url = require("./wiki_url");
 const base = Date.now();
-(async () => {
-	while (true) {
+async function request() {
+	for (let i = 0; i < 30; i++) {
 		try {
-			let [official, wiki] = await Promise.all([
+			return await Promise.all([
 				axios.get("https://webapi.lowiro.com/webapi/serve/static/bin/arcaea/apk").then(v => v.data.value),
 				axios.get(wiki_url({ titles: "Template:version" })).then(v => v.data.query.pages[0].revisions[0].content),
 			])
-			let newVersion = official.version.replace(/c$/, "")
-			let result = wiki.replace(/(?<=mobile=v)\S+/, newVersion)
-			// if (wiki != result) {
-			if (wiki != result || process.argv[2] == 0) {
-				console.log(official.url)
-				console.log(official.version)
-				console.log(result)
-				return
-			}
-			if (Date.now() - base >= 1000 * 60 * process.argv[2]) {
-				console.error("\033[;31mtimeOut\033[0m")
-				process.exit(1)
-			}
-			console.warn("sleeping")
-			await new Promise(resolve => setTimeout(resolve, 1000 * 60))
-		} catch { continue }
+		} catch { await new Promise(resolve => setTimeout(resolve, 1000)) }
+	}
+	unexpected()
+}
+function unexpected() {
+	console.error("\033[;31mtimeOut\033[0m")
+	process.exit(1)
+}
+(async () => {
+	while (true) {
+		let [official, wiki] = await request()
+		let newVersion = official.version.replace(/c$/, "")
+		let result = wiki.replace(/(?<=mobile=v)\S+/, newVersion)
+		// if (wiki != result) {
+		if (wiki != result || process.argv[2] == 0) {
+			console.log(official.url)
+			console.log(official.version)
+			console.log(result)
+			return
+		}
+		if (Date.now() - base >= 1000 * 60 * process.argv[2]) unexpected()
+		console.warn("sleeping")
+		await new Promise(resolve => setTimeout(resolve, 1000 * 60))
 	}
 })()
